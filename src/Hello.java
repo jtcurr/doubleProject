7eb4360c4220a12e23baa1d7855c923711bf2340
John Currie <johntcurrie@gmail.com>
2017-07-20 10:52:24 -0700

    Building out initial game display

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Testing hello class");
+
+        int mynumber = 5;
+        mynumber = 7;
+        System.out.println(mynumber);
     }
 }
